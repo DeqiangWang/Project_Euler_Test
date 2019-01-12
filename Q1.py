501650c604381f7eb8792654164bd874224f32a0
widyaageng <ageng.setyatutuko@gmail.com>
2019-01-12 18:55:38 +0800

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,15 @@
+# Euler 1
+
+
+def iter_mod(n, modulo):
+    for x in range(n):
+        y = (x+1) % modulo
+        if y == 0:
+            yield (x+1)
+
+
+num_3 = list(iter_mod(999, 3))
+num_5 = list(iter_mod(999, 5))
+
+multiples = list(set(num_3 + num_5))
+print(sum(multiples))
