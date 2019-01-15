db2020422ccfdeaea5ab71ec925aa5d6d2237d44
widyaageng <ageng.setyatutuko@gmail.com>
2019-01-15 23:43:35 +0800

    Restructure

--- EvenFibonacciNumbers.py
+++ EvenFibonacciNumbers.py
@@ -9,17 +9,17 @@ def fib_seq(limit):
         x.append(x[-2] + x[-1])
         if x[-1] <= limit:
             yield x[-1]
-            
+
 
 def array_evsum(self):
     even_arr_sum = 0
     for x in range(self.__len__()):
-        if self[x]%2 == 0:
+        if self[x] % 2 == 0:
             even_arr_sum = even_arr_sum + self[x]
         else:
             pass
     return even_arr_sum
-    
-    
+
+
 fibnum = list(fib_seq(4000000))
 print(array_evsum(fibnum))
