90a0797f60307dd7a5b6cbbc52349e906dc3f0c1
Iki_piye <39331919+widyaageng@users.noreply.github.com>
2019-01-15 12:33:54 +0800

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,11 +1,25 @@
 # Euler 2
 
 
-def iter_fib(stop):
-    if stop < 3:
-        return 3
-    else:
-        return iter_fib(prev_num0, prev_num1 + prev_num0, stop-1)
+def fib_seq(limit):
+    x = [1, 2];
+    yield 1
+    yield 2
+    while x[-1] < limit:
+        x.append(x[-2] + x[-1])
+        if x[-1] <= limit:
+            yield x[-1]
+            
 
-
-print(iter_fib())
+def array_evsum(self):
+    even_arr_sum = 0
+    for x in range(self.__len__()):
+        if self[x]%2 == 0:
+            even_arr_sum = even_arr_sum + self[x]
+        else:
+            pass
+    return even_arr_sum
+    
+    
+fibnum = list(fib_seq(35))
+print(array_evsum(fibnum))
