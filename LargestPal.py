33e0cbfde52f6d367cb6e15ef5941e7c88b7b937
Iki_piye <39331919+widyaageng@users.noreply.github.com>
2019-02-02 11:37:55 +0800

    init e4

--- LargestPal.py
+++ LargestPal.py
@@ -2,12 +2,13 @@
 
 import math
 
+
 def ispalindrome(num):
     stringnum = str(num)
-    for i in range(math.floor(stringnum.__len__()/2)):
-        if not stringnum[i] == stringnum[(-1*i - 1)]:
+    for i in range(math.floor(stringnum.__len__() / 2)):
+        if not stringnum[i] == stringnum[(-1 * i - 1)]:
             return False
     return True
-    
+
 
 print(ispalindrome(758857))
