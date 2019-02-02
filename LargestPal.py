3b7be9902af3d391ada883c77242c9ba6e06cd53
Iki_piye <39331919+widyaageng@users.noreply.github.com>
2019-02-02 14:16:38 +0800

    Push Euler 4
    Init Eul 4 problem

--- LargestPal.py
+++ LargestPal.py
@@ -1,6 +1,7 @@
 # Euler 4
 
 import math
+import time
 
 
 def ispalindrome(num):
@@ -11,4 +12,24 @@ def ispalindrome(num):
     return True
 
 
-print(ispalindrome(758857))
+def digit_count(num):
+    return int(len(str(num)))
+
+
+# main
+def main():
+    for max_pal_candidate in range(999 * 999, 100 * 100, -1):
+        if ispalindrome(max_pal_candidate):
+            # the fact that palindrome of 6 digits/5 digits are divisible by 11
+            for divisor in range(110, 990, 11):
+                if (max_pal_candidate % divisor) == 0:
+                    if digit_count(int(max_pal_candidate / divisor)) == 3:
+                        print(max_pal_candidate)
+                        return
+
+
+start_time = time.time()
+main()
+print("Time elapsed:" + str(time.time() - start_time))
+eul4 = "YnkgV2lkeWEgQWdlbmcgU2V0eWEgVHV0dWtv"
+
